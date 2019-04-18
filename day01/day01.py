e2ae14974c78240c4176fcc008cdae967be1382a
Zeron <38061829+ZuhongLi@users.noreply.github.com>
2019-04-18 08:54:27 +0800

    Create day01.py

---
+++ day01/day01.py
@@ -0,0 +1,43 @@
+'''
+@author:Lee
+@file:test01.py
+@Time: 2019/3/25 19:53
+'''
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+#         res = [];
+#         for i in range(len(nums)):
+#             for j in range(i+1,len(nums)):
+#                 if nums[i] == target - nums[j]:
+#                     res.append(i);
+#                     res.append(j);
+#         return res;
+# s = Solution().twoSum([2,3,5],7)
+# print(s)
+        if len(nums) <= 1:
+            return False
+        buff_dict = {}
+        for i in range(len(nums)):
+            if nums[i] in buff_dict:
+                return [buff_dict[nums[i]], i]
+            else:
+                buff_dict[target - nums[i]] = i
+                print(buff_dict)
+s = Solution().twoSum([2,3,5,7,9,11,13,15,17],19)
+print(s)
+
+#         if len(nums)<= 1:
+#             return None
+#         buff_dict = {};
+#         for i in range(len(nums)):
+#             if nums[i] in buff_dict:
+#                 return [buff_dict[nums[i]],i]
+#             else:
+#                 buff_dict[target-nums[i]] = i;
+# s= Solution().twoSum([2,3,5,7,9,11,13,15,17],20)
+# print(s)
